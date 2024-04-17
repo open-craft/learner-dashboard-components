06a52e97bbd049e2763669de3b89abc3398ee428
Navin Karkera <navin@opencraft.com>
2024-04-17 19:11:19 +0530

    feat: sidebar filter component

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,122 @@
+import { Button, ButtonGroup, Form, Stack } from "@openedx/paragon";
+
+const Sidebar = ({
+  btnGroups,
+  statusOptions,
+  subjectOptions,
+  durationOptions,
+  activeFilters,
+  setActiveFilters,
+}) => {
+  const renderFilterBtns = () => {
+    return btnGroups.map((btn) => (
+      <Button
+        id={btn.id}
+        variant={activeFilters.type === btn.id ? "brand" : "outline-brand"}
+        className={activeFilters.type !== btn.id && "text-gray-700 border-medium-gray"}
+        onClick={() => setActiveFilters((prev) => ({ ...prev, type: btn.id }))}
+      >
+        {btn.text}
+      </Button>
+    ));
+  }
+
+  const renderStatusRadio = () => {
+    const handleChange = e => setActiveFilters(
+      (prev) => ({ ...prev, status: e.target.value })
+    );
+    return (
+      <Form.Group>
+        <Form.Label
+          className="font-weight-bold text-black mb-3"
+        >
+          Status
+        </Form.Label>
+        <Form.RadioSet
+          name="status"
+          onChange={handleChange}
+          value={activeFilters.status}
+        >
+          {statusOptions.map((status) => (
+            <Form.Radio
+              controlClassName="border-black"
+              value={status.id}
+            >
+              {status.text}
+            </Form.Radio>
+          ))}
+        </Form.RadioSet>
+      </Form.Group>
+    );
+  };
+
+  const renderChecboxes = (label, key, options) => {
+    const handleChange = e => {
+      if (e.target.checked) {
+        setActiveFilters((prev) => ({
+          ...prev,
+          [key]: [...prev[key], e.target.value],
+        }));
+      } else {
+        setActiveFilters((prev) => ({
+          ...prev,
+          [key]: prev[key].filter(val => val !== e.target.value),
+        }));
+      }
+    }
+
+    return (
+      <Form.Group>
+        <Form.Label
+          className="font-weight-bold text-black mb-3"
+        >
+          {label}
+        </Form.Label>
+        <Form.CheckboxSet
+          name={label.toLowerCase()}
+          onChange={handleChange}
+          value={activeFilters[key]}
+        >
+          {options.map((option) => (
+            <Form.Checkbox value={option.id}>
+              {option.text}
+            </Form.Checkbox>
+          ))}
+        </Form.CheckboxSet>
+      </Form.Group>
+    );
+  }
+
+  const handleClearAll = () => {
+    setActiveFilters({
+      type: "all",
+      status: "",
+      subjects: [],
+      durations: [],
+    });
+  }
+
+  return (
+    <Stack gap={3}>
+      <div className="d-flex justify-content-between x-small">
+        <b className="text-black lead">Filter</b>
+        <Button
+          variant="link"
+          size="inline"
+          className="text-dark-turquoise text-decoration-underline"
+          onClick={handleClearAll}
+        >
+          Clear all
+        </Button>
+      </div>
+      <ButtonGroup className="mb-2">
+        {renderFilterBtns()}
+      </ButtonGroup>
+      {renderStatusRadio()}
+      {renderChecboxes("Subject", "subjects", subjectOptions)}
+      {renderChecboxes("Duration", "durations", durationOptions)}
+    </Stack>
+  )
+}
+
+export default Sidebar;
